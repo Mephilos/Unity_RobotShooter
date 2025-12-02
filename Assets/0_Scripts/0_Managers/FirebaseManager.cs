ed846bd51362cb9f993f8a6cd9326fe1cca1b467
Mephilos <sth4628@hotmail.com>
2025-12-02 13:45:01 +0900

    firebaseManager.cs: 파이어베이스 연동,포멧 작성 / PlaySceneUI.cs: 점수 기록 기본사항 적용

--- Assets/0_Scripts/0_Managers/FirebaseManager.cs
+++ Assets/0_Scripts/0_Managers/FirebaseManager.cs
@@ -1,16 +1,151 @@
 using UnityEngine;
+using Firebase;
+using Firebase.Auth;
+using Firebase.Database;
+using Firebase.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+[Serializable]
+public class UserScoreData
+{
+    public int score;
+    public float time;
+    public float acc;
+
+    public UserScoreData(int score, float time, float acc)
+    {
+        this.score = score;
+        this.time = time;
+        this.acc = acc;
+    }
+}
 
 public class FirebaseManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static FirebaseManager Instance;
+    FirebaseAuth firebaseAuth;
+    DatabaseReference databaseReference;
+
+    string userId;
+
+    public int BestScore { get; private set; } = 0;
+    public float BestTime { get; private set; } = 9999f;
+    public float BestAcc { get; private set; } = 0f;
+
+    public bool isDataLoad = false;
+
+    void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        InitializeFirebase();
+    }
+
+    void InitializeFirebase()
+    {
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task =>
+        {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == DependencyStatus.Available)
+            {
+                firebaseAuth = FirebaseAuth.DefaultInstance;
+                databaseReference = FirebaseDatabase.DefaultInstance.RootReference;
+                SignInAnonymously();
+            }
+            else
+            {
+                Debug.LogError($"파이어베이스 연결 실패: {dependencyStatus}");
+            }
+        });
+    }
+
+    void SignInAnonymously()
+    {
+        firebaseAuth.SignInAnonymouslyAsync().ContinueWithOnMainThread(task =>
+        {
+            if (task.IsCanceled || task.IsFaulted)
+            {
+                Debug.LogError("익명 로그인 실패");
+                return;
+            }
+
+            FirebaseUser newUser = task.Result.User;
+            userId = newUser.UserId;
+            Debug.Log($"로그인 완료 ID: {userId}");
+
+            LoadMyData();
+        });
+    }
+    void LoadMyData()
+    {
+        if (string.IsNullOrEmpty(userId)) return;
+
+        databaseReference.Child("users").Child(userId).GetValueAsync().ContinueWithOnMainThread(task =>
+        {
+            if (task.IsCompleted)
+            {
+                DataSnapshot dataSnapshot = task.Result;
+                if (dataSnapshot.Exists && dataSnapshot.HasChildren)
+                {
+                    IDictionary<string, object> data = (IDictionary<string, object>)dataSnapshot.Value;
+                    if (data.ContainsKey("score")) BestScore = Convert.ToInt32(data["score"]);
+                    if (data.ContainsKey("time")) BestTime = Convert.ToSingle(data["time"]);
+                    if (data.ContainsKey("score")) BestAcc = Convert.ToSingle(data["acc"]);
+
+                    Debug.Log($"기록 로드 완료| 점수: {BestScore}, 시간: {BestTime}, 정확도:{BestAcc}");
+                }
+                else
+                {
+                    Debug.Log("신규 유저");
+                }
+                isDataLoad = true;
+            }
+        });
+    }
+    public void RenewScore(int currentScore, float currentTime, float currentAcc)
+    {
+        if (string.IsNullOrEmpty(userId)) return;
+
+        bool isNewScore = false;
+
+        if (currentScore > BestScore)
+        {
+            BestScore = currentScore;
+            isNewScore = true;
+        }
+
+        else if (currentScore == BestScore && currentTime < BestTime)
+        {
+            BestTime = currentTime;
+            isNewScore = true;
+        }
+
+        if (currentAcc > BestAcc)
+        {
+            BestAcc = currentAcc;
+        }
+
+        if (isNewScore)
+        {
+            UserScoreData userScoreData = new UserScoreData(BestScore, BestTime, BestAcc);
+            string json = JsonUtility.ToJson(userScoreData);
+
+            databaseReference.Child("users").Child(userId).SetRawJsonValueAsync(json);
+            Debug.Log("서버에 신기록 갱신 완료");
+        }
     }
 }
--- Assets/0_Scripts/_UI/PlaySceneUI.cs
+++ Assets/0_Scripts/_UI/PlaySceneUI.cs
@@ -1,25 +1,33 @@
 using UnityEngine;
 using TMPro;
 using StarterAssets;
+using UnityEngine.UI;
 
 public class PlaySceneUI : MonoBehaviour
 {
     [SerializeField] TMP_Text enemiesLeftText;
     [SerializeField] TMP_Text scoreText;
     [SerializeField] TMP_Text accText;
-    [SerializeField] GameObject winText;
+    [SerializeField] GameObject winContainer;
+    [SerializeField] GameObject scorePanel;
+
+    [SerializeField] TMP_Text finalScoreText;
+    [SerializeField] TMP_Text finalTimeText;
+    [SerializeField] TMP_Text finalAccText;
+
     StarterAssetsInputs starterAssetsInputs;
 
     void Start()
     {
         starterAssetsInputs = FindFirstObjectByType<StarterAssetsInputs>();
+
         LevelManager.Instance.OnEnemyCountChanged += UpdateEnemyLeft;
         LevelManager.Instance.OnLevelWin += ShowWinUI;
         UpdateEnemyLeft(LevelManager.Instance.GetEnemiesCount());
 
         ScoreManager.Instance.OnScoreChanged += UpdateScoreUI;
-        UpdateScoreUI(ScoreManager.Instance.GetCurrentScore());
         ScoreManager.Instance.OnAccChanged += UpdateAccUI;
+        UpdateScoreUI(ScoreManager.Instance.GetCurrentScore());
         UpdateAccUI(ScoreManager.Instance.GetAccuracy());
     }
     void OnDestroy()
@@ -29,25 +37,109 @@ public class PlaySceneUI : MonoBehaviour
         ScoreManager.Instance.OnScoreChanged -= UpdateScoreUI;
         ScoreManager.Instance.OnAccChanged -= UpdateAccUI;
     }
+    void ShowWinUI()
+    {
+        winContainer.SetActive(true);
+
+        int currentScore = ScoreManager.Instance.GetCurrentScore();
+        float currentAcc = ScoreManager.Instance.GetAccuracy();
+        float currentTime = Time.timeSinceLevelLoad;
+
+        int bestScore = FirebaseManager.Instance.BestScore;
+        float bestTime = FirebaseManager.Instance.BestTime;
+        float bestAcc = FirebaseManager.Instance.BestAcc;
+
+        DisplayComparisonScore(finalScoreText, "SCORE", currentScore, bestScore, true);
+        DisplayComparisonTime(finalTimeText, currentTime, bestTime);
+        DisplayComparisonScore(finalAccText, "ACCURACY", currentAcc, bestAcc, true, "%");
+
+        FirebaseManager.Instance.RenewScore(currentScore, currentTime, currentAcc);
+
+        UnlockCursor();
+    }
+
+    void DisplayComparisonScore(TMP_Text textUI, string label, float current, float best, bool isBetter, string suffix = "")
+    {
+        float valueDiff = current - best;
+        string comparisonString = "";
+        string resultColor = "white";
+
+        if (best == 0 || best > 9000)
+        {
+            comparisonString = " (New Record)";
+            resultColor = "yellow";
+        }
+        else
+        {
+            if (Mathf.Abs(valueDiff) < 0.01f)
+            {
+                comparisonString = " (-)";
+                resultColor = "#888888";
+            }
+            else if ((isBetter && valueDiff > 0) || (!isBetter && valueDiff < 0))
+            {
+                comparisonString = $" (+{Mathf.Abs(valueDiff):0.##}{suffix})";
+
+                if (!isBetter)
+                {
+                    comparisonString = $" (-{Mathf.Abs(valueDiff):0.##}{suffix})";
+                }
+                resultColor = "#00FF00";
+            }
+            else
+            {
+                comparisonString = $" ({valueDiff:0.##}{suffix})";
+                resultColor = "#FF0000";
+            }
+        }
+        textUI.text = $"{label}: {current:0.##}{suffix} <color={resultColor}>{comparisonString}</color>";
+    }
+
+    void DisplayComparisonTime(TMP_Text textUI, float currentTime, float bestTime)
+    {
+        string timeStr = string.Format("{0:00}:{1:00}", (int)currentTime / 60, (int)currentTime % 60);
+
+        float timeDiff = currentTime - bestTime;
+        string comparisonString = "";
+        string resultColor = "white";
+
+        if (bestTime > 9000)
+        {
+            comparisonString = "(New)";
+            resultColor = "yellow";
+        }
+        else if (timeDiff < 0)
+        {
+            comparisonString = $"(-{Mathf.Abs(timeDiff):0}s)";
+            resultColor = "#00FF00";
+        }
+        else
+        {
+            comparisonString = $"(+{timeDiff:0}s)";
+            resultColor = "#FF0000";
+        }
+        textUI.text = $"TIME: {timeStr} <color={resultColor}>{comparisonString}</color>";
+    }
+
     void UpdateEnemyLeft(int count)
     {
         enemiesLeftText.text = Constants.ENEMIES_LEFT_STRING + count.ToString("D2");
     }
+
     void UpdateScoreUI(int score)
     {
         scoreText.text = Constants.SCORE_STRING + $"{score:N0}";
     }
+
     void UpdateAccUI(float acc)
     {
         accText.text = Constants.ACC_STRING + $"{acc:F1}%";
     }
-    void ShowWinUI()
-    {
-        winText.SetActive(true);
 
+    void UnlockCursor()
+    {
         starterAssetsInputs.SetInputBlocked(true);
         starterAssetsInputs.SetCursorState(false);
-
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
     }
